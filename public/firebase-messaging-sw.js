e35cf724ecfe6521a7d73abbaa2de2e6ed3d8bec
Innocent Makaya <innocentmakayat@gmail.com>
2025-03-14 16:57:55 +0200

    Vercel Notification V1

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,17 +1,18 @@
-// firebase-messaging-sw.js
 importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
 
+// Use environment variables injected at build time
 const firebaseConfig = {
-    apiKey: "AIzaSyAqMHe8MB8xwe77nJmJ5sCBBIWNEjxMgmk",
-    authDomain: "stelliesmarketnotifications.firebaseapp.com",
-    projectId: "stelliesmarketnotifications",
-    storageBucket: "stelliesmarketnotifications.firebasestorage.app",
-    messagingSenderId: "787238005907",
-    appId: "1:787238005907:web:28cff7a131584cc12b7166",
-    measurementId: "G-3J5JW1NFZB"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
